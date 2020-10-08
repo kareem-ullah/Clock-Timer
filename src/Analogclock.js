c1350ef80f9683d229731952013d656b024e4027
kareem-ullah <kareemullah.ayub.14@gmail.com>
2020-10-08 16:15:30 +0500

    changes

--- src/Analogclock.js
+++ src/Analogclock.js
@@ -4,32 +4,28 @@ import { Container, Row, Col } from 'react-bootstrap';
 
 function Analogclock(props) {
     let time = new Date(props.time);
-    console.log(time.getHours(), 'Analog', (time.getHours()/30) * 360 - 90);
+    console.log(time.getHours(), 'Analog', (time.getHours() / 30) * 360 - 90);
 
-    let divclock = {
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
+
 
 
     let clockContainer = {
-        width: 200,
-        height: 200,
+        width: 150,
+        height: 150,
         borderRadius: 20000,
         // borderStyle: "solid",
         // borderColor: "red",
-        border: '40px solid #cddc39',
-        backgroundColor: ''
+        border: '2px solid #697689',
+        backgroundColor: 'white'
 
     }
 
     let secondHand = {
         position: 'relative',
         top: -10,
-        left: 100,
+        left: 77,
         border: '1px solid red',
-        width: 120,
+        width: 100,
         height: 1,
         transform: 'rotate(' + ((time.getSeconds() / 60) * 360 - 90).toString() + 'deg)',
         transformOrigin: '0% 0%',
@@ -39,10 +35,10 @@ function Analogclock(props) {
 
     let minuteHand = {
         position: 'relative',
-        top: -17,
-        left: 98,
+        top: -11,
+        left: 75,
         border: '2px solid #cddc39',
-        width: 95,
+        width: 110,
         height: 3,
         borderRadius: '10px',
         // transform: 'rotate(' + ((time.getMinutes()/60)*360 - 90 ).toString() + 'deg)',
@@ -54,38 +50,41 @@ function Analogclock(props) {
 
     let hourHand = {
         position: 'relative',
-        top: -16,
-        left: 99,
+        top: -25,
+        left: 75,
         border: '2px solid #cddc39',
-        width: 70,
+        width: 95,
         height: 6,
         transform: 'rotate(' + ((time.getHours() / 12) * 360 - 90).toString() + 'deg)',
         transformOrigin: '0% 0%',
-        // backgroundColor: '#cddc39',
+        backgroundColor: '#cddc39',
         borderRadius: '10px',
         zIndex: '-1'
     }
 
     return (
-        <div style={divclock}>
-            <Container style={clockContainer}>
-            <p className="p12">12</p>
-            <p className="p1">1</p>
-            <p className="p2">2</p>
-            <p className="p3">3</p>
-            <p className="p4">4</p>
-            <p className="p5">5</p>
-            <p className="p6">6</p>
-            <p className="p7">7</p>
-            <p className="p8">8</p>
-            <p className="p9">9</p>
-            <p className="p10">10</p>
-            <p className="p11">11</p>
-            <div className="divmin"></div>
-                <div style={secondHand}></div>
-                <div style={minuteHand}></div>
-                <div style={hourHand}></div>
-            </Container>
+        <div className="divclock">
+            <div className="divnum">
+                <p className="p12">12</p>
+                <p className="p1">1</p>
+                <p className="p2">2</p>
+                <p className="p3">3</p>
+                <p className="p4">4</p>
+                <p className="p5">5</p>
+                <p className="p6">6</p>
+                <p className="p7">7</p>
+                <p className="p8">8</p>
+                <p className="p9">9</p>
+                <p className="p10">10</p>
+                <p className="p11">11</p>
+                <Container style={clockContainer}>
+                    <h5>SHADAB</h5>
+                    <div className="divmin"></div>
+                    <div style={secondHand}></div>
+                    <div style={minuteHand}></div>
+                    <div style={hourHand}></div>
+                </Container>
+            </div>
         </div>
     )
 }
